54f2e70270f567c3c9c8b3f18ba9d55d4a17c2de
cristian2024 <cristiandramirezm.2024@gmail.com>
2021-01-17 10:27:26 -0500

    g

---
+++ paginas web/primera_clase/js/index.js
@@ -0,0 +1,12 @@
+var button=document.getElementById("ave");
+
+//button.addEventListener("click",trabajo);
+
+
+
+//alert("hola");
+function trabajo(){
+    alert("Adios");
+}
+
+window.onload(trabajo);
